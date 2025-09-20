e4f700e5a7c1dc633f0c4ef56c14f36111953559
fuadbaqi910 <fuadbaqi910@gmail.com>
2025-09-20 12:03:33 +0000

    pertemuan 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,48 @@
-
 import 'package:flutter/material.dart';
-// import 'package:myapp/column_widget.dart';
-// import 'package:myapp/row_widget.dart';
-import 'package:myapp/basic_widget.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
 
+void main() => runApp(const JustduitApp());
 
-void main() {
-  runApp(const MyApp());
+class JustduitApp extends StatelessWidget {
+  const JustduitApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Justduit',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF3F6F8), // abu-abu muda
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF1E88FF),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          isDense: true,
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding:
+              const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFDEE0E0)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFFDEE0E0)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: const BorderSide(color: Color(0xFF1E88FF)),
+          ),
+        ),
+      ),
+      initialRoute: LoginScreen.route,
+      routes: {
+        LoginScreen.route: (context) => const LoginScreen(), // default = Sign In
+        SignupScreen.route: (context) => const SignupScreen(),
+      },
+    );
   }
-
-  class MyApp extends StatelessWidget {
-    const MyApp({super.key});
-
-      // This widget is the root of your application.
-        @override
-          Widget build(BuildContext context) {
-              return MaterialApp(
-                    title: 'Praktikum Mobile Lanjut',
-                          theme: ThemeData(
-                                  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-                                          useMaterial3: true,
-                                                ),
-                                                      home: const BasicWidget(),
-                                                          );
-                                                            }
-                                                            }
+}
