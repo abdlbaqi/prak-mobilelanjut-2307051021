7151963b3e072f9fe3eefe35faaf73200163e7be
fuadbaqi910 <fuadbaqi910@gmail.com>
2025-10-07 13:59:42 +0000

    menambahkan splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
 import 'screens/dashboard_screen.dart';
+import 'screens/splashscreen.dart';
 
 void main() => runApp(const JustduitApp());
 
@@ -44,6 +45,7 @@ class JustduitApp extends StatelessWidget {
         LoginScreen.route: (context) => const LoginScreen(), // default = Sign In
         SignupScreen.route: (context) => const SignupScreen(),
          DashboardScreen.route: (context) => const DashboardScreen(),
+         SplashScreen.route: (context) => const SplashScreen(),
       },
     );
   }
